708c9ecffa38052c92a82c9b2e04cd886d73b869
George <prich845@gmail.com>
2022-10-18 13:11:43 +0300

    first commit

---
+++ app/src/main/java/com/magpie/newsapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.magpie.newsapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
