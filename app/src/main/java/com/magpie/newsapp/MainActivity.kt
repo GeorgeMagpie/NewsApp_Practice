10034ce50c751a7bcb1c962414da585ea5a946fd
George <prich845@gmail.com>
2022-10-18 21:13:25 +0300

    lesson 2 Все еще не понимаю. как что работает - до конца, но я разберусь))

--- app/src/main/java/com/magpie/newsapp/MainActivity.kt
+++ app/src/main/java/com/magpie/newsapp/MainActivity.kt
@@ -2,10 +2,30 @@ package com.magpie.newsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.magpie.newsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private  var _binding: ActivityMainBinding? = null
+    private val mBinding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(R.layout.fragment_splash)
+        Handler(Looper.myLooper()!!).postDelayed({
+            setContentView(mBinding.root)
+            mBinding.bottomNavMenu.setupWithNavController(
+                navController = mBinding.navHostFragment.findNavController()
+            )
+        }, 4000)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
---
+++ app/src/main/java/com/magpie/newsapp/ui/search/SearchFragment.kt
@@ -0,0 +1,21 @@
+package com.magpie.newsapp.ui.search
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.magpie.newsapp.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+    private var _binding: FragmentSearchBinding? = null
+    private val mBinding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
+        return mBinding.root
+    }
+}
